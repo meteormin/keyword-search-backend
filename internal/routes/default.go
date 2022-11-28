f7bd3d35e2c7880510304a64dc90de61d57be1e5
miniyus <miniyu97@gmail.com>
2022-11-29 01:18:19 +0900

    fix: 구조 변경으로 인한 자동 수정된 소스 수정

--- internal/routes/default.go
+++ internal/routes/default.go
@@ -1,14 +1,14 @@
 package routes
 
 import (
-	api_auth2 "github.com/miniyus/go-fiber/internal/app/api_auth"
-	test_api2 "github.com/miniyus/go-fiber/internal/app/test_api"
-	users2 "github.com/miniyus/go-fiber/internal/app/users"
+	"github.com/miniyus/go-fiber/internal/app/api_auth"
+	"github.com/miniyus/go-fiber/internal/app/test_api"
+	"github.com/miniyus/go-fiber/internal/app/users"
 	"github.com/miniyus/go-fiber/internal/core/container"
 	"github.com/miniyus/go-fiber/pkg/jwt"
 )
 
-var Prefix = "/api"
+const Prefix = "/api"
 
 func SetRoutes(container container.Container) {
 	api := container.App().Group(Prefix)
@@ -16,12 +16,12 @@ func SetRoutes(container container.Container) {
 	var tokenGenerator jwt.Generator
 
 	container.Resolve(&tokenGenerator)
-	api_auth2.Register(api, api_auth2.Factory(container.Database(), tokenGenerator))
+	api_auth.Register(api, api_auth.Factory(container.Database(), tokenGenerator))
 
-	test_api2.Register(api, test_api2.Factory(container.Database()))
+	test_api.Register(api, test_api.Factory(container.Database()))
 
 	// need jwt token
 
-	users2.Register(api, users2.Factory(container.Database()))
+	users.Register(api, users.Factory(container.Database()))
 
 }
