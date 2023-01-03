2d9cba1a70b2fe6ca7e67e41c35af3fcdf447c10
miniyus <miniyu97@gmail.com>
2023-01-04 03:15:21 +0900

    feat: groups api route

--- internal/app/groups/routes.go
+++ internal/app/groups/routes.go
@@ -9,6 +9,11 @@ const Prefix = "/groups"
 
 func Register(handler Handler) router.Register {
 	return func(router fiber.Router) {
-
+		router.Get("/", handler.All).Name("api.groups.all")
+		router.Get("/:id", handler.Find).Name("api.groups.find")
+		router.Get("/name/:name", handler.FindByName).Name("api.groups.find-by-name")
+		router.Put("/:id", handler.Update).Name("api.groups.update")
+		router.Patch("/:id", handler.Patch).Name("api.groups.patch")
+		router.Delete("/:id", handler.Delete).Name("api.groups.delete")
 	}
 }
