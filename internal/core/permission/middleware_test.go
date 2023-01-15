ecbe71eb7be2d788d22580462959119a8d2bcb8d
miniyus <miniyu97@gmail.com>
2023-01-15 15:26:51 +0900

    test: check permission

---
+++ internal/core/permission/middleware_test.go
@@ -0,0 +1,51 @@
+package permission_test
+
+import (
+	"github.com/miniyus/keyword-search-backend/internal/core/permission"
+	"github.com/miniyus/keyword-search-backend/internal/utils"
+	"strings"
+	"testing"
+)
+
+var hasPerm = []permission.Permission{
+	{
+		Actions: []permission.Action{
+			{
+				Methods: []permission.Method{
+					"GET",
+				},
+				Resource: "/test",
+			},
+		},
+		GroupId: 1,
+		Name:    "TEST",
+	},
+}
+
+func TestCheckPermission(t *testing.T) {
+	pass := false
+	method := "GET"
+	utils.NewCollection(hasPerm).For(func(perm permission.Permission, i int) {
+		utils.NewCollection(perm.Actions).For(func(action permission.Action, j int) {
+			routePath := "/test"
+			if strings.Contains(routePath, action.Resource) {
+
+				if method == "OPTION" {
+					method = "GET"
+				}
+
+				filtered := utils.NewCollection(action.Methods).Filter(func(v permission.Method, i int) bool {
+					return string(v) == method
+				})
+
+				if len(filtered) != 0 {
+					pass = true
+				}
+			}
+		})
+	})
+
+	if !pass {
+		t.Error(pass)
+	}
+}
