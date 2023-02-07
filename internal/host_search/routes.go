e226d7a33670cfadc82f93fc57e9ff1c4f26410c
miniyus <miniyu97@gmail.com>
2023-02-08 00:42:58 +0900

    chore: 패키지 업데이트(버그 픽스)

--- internal/host_search/routes.go
+++ internal/host_search/routes.go
@@ -3,17 +3,15 @@ package host_search
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/miniyus/gofiber/app"
-	"github.com/miniyus/gofiber/permission"
 )
 
 const Prefix = "/hosts/:id/search"
 
-func Register(handler Handler, hasPerm permission.HasPermissionParameter) app.SubRouter {
+func Register(handler Handler) app.SubRouter {
 	return func(router fiber.Router) {
 		router.Get("/", handler.GetByHostId).Name("api.hosts.id.search")
 		router.Get("/descriptions", handler.GetDescriptionsByHostId).Name("api.hosts.id.search.descriptions")
 		router.Post("/",
-			permission.HasPermission(hasPerm),
 			handler.BatchCreate,
 		).Name("api.hosts.batch-create")
 	}
--- routes/api.go
+++ routes/api.go
@@ -4,10 +4,8 @@ import (
 	"github.com/miniyus/gofiber/app"
 	configure "github.com/miniyus/gofiber/config"
 	"github.com/miniyus/gofiber/database"
-	"github.com/miniyus/gofiber/permission"
 	"github.com/miniyus/gofiber/pkg/worker"
 	"github.com/miniyus/gofiber/utils"
-	"github.com/miniyus/keyword-search-backend/internal/group_detail"
 	"github.com/miniyus/keyword-search-backend/internal/host_search"
 	"github.com/miniyus/keyword-search-backend/internal/hosts"
 	"github.com/miniyus/keyword-search-backend/internal/search"
@@ -36,15 +34,6 @@ func Api(apiRouter app.Router, a app.Application) {
 	var jDispatcher worker.Dispatcher
 	a.Resolve(&jDispatcher)
 
-	permissions := permission.NewPermissionsFromConfig(cfg.Permission)
-	permissionCollection := permission.NewPermissionCollection(permissions...)
-
-	hasPermParam := permission.HasPermissionParameter{
-		DB:           db,
-		DefaultPerms: permissionCollection,
-		FilterFunc:   group_detail.FilterFunc,
-	}
-
 	apiRouter.Route(
 		hosts.Prefix,
 		hosts.Register(hosts.New(db)),
@@ -59,7 +48,7 @@ func Api(apiRouter app.Router, a app.Application) {
 
 	apiRouter.Route(
 		host_search.Prefix,
-		host_search.Register(hostSearchHandler, hasPermParam),
+		host_search.Register(hostSearchHandler),
 	).Name("api.hosts.search")
 
 	apiRouter.Route(
